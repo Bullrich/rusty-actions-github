51bd1d7f2607072c0a19c47673f7ef5ac1bdab72
Javier Bullrich <javier@bullrich.dev>
2024-05-24 18:09:23 +0200

    documented method

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,19 @@
 use std::env;
 
 use crate::error::ActionsError;
-pub fn get_input(name:&str)-> Result<String,ActionsError> {    let clean_input = str::replace(name, ' ', "_");    let value = env::var(clean_input.to_uppercase());    match value {        Ok(input) => Ok(input),        Err(_) => Err(ActionsError::InputNotFound(name.to_string()))    }}
+
+// Implemented from https://github.com/actions/toolkit/blob/main/packages/core/src/core.ts#L126
+/// Obtain an input from a variable
+///
+/// If the input is not found, an [ActionsError] is returned
+///
+/// This method was copied
+/// from [core.ts](https://github.com/actions/toolkit/blob/d1df13e178816d69d96bdc5c753b36a66ad03728/packages/core/src/core.ts#L126)
+pub fn get_input(name:&str)-> Result<String,ActionsError> {
+    let clean_input = str::replace(name, ' ', "_");
+    let value = env::var(clean_input.to_uppercase());
+    match value {
+        Ok(input) => Ok(input),
+        Err(_) => Err(ActionsError::InputNotFound(name.to_string()))
+    }
+}
