08fb1001ac6341e74159b2ba6ca6dfeea3c2b3ee
Javier Bullrich <javier@bullrich.dev>
2024-05-27 15:59:47 +0200

    added parameters to logs
    Now the users can point to specific failing lines

--- src/logger.rs
+++ src/logger.rs
@@ -1,11 +1,44 @@
 //! Logging methods for the output
+use std::fmt::{Display, Formatter};
 use std::io::Write;
 use std::{env, io};
 
 use crate::util::EOL;
 
-fn issue_command(command: &str, msg: &str) {
-    let message = format!("::{} {}", command, msg);
+#[derive(Debug)]
+pub struct LogParameters {
+    title: String,
+    file: String,
+    line: u16,
+    end_line: u16,
+}
+
+impl Display for LogParameters {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "file={},line={},endLine={},title={}",
+            self.file, self.line, self.end_line, self.title
+        )
+    }
+}
+
+impl Default for LogParameters {
+    fn default() -> Self {
+        LogParameters {
+            title: "".to_string(),
+            file: ".github".to_string(),
+            line: 1,
+            end_line: 1,
+        }
+    }
+}
+
+fn issue_command(command: &str, msg: &str, parameters: Option<LogParameters>) {
+    let message = match parameters {
+        None => format!("::{}::{}", command, msg),
+        Some(params) => format!("::{} {}::{}", command, params, msg),
+    };
     io::stdout()
         .write_all((message + EOL).as_bytes())
         .expect("Failed to write message")
@@ -44,10 +77,10 @@ pub fn info(msg: &str) {
 ///
 /// ```rust
 /// use actions_github::logger;
-/// logger::warn_log("Missing name of project");
+/// logger::warn_log("Missing name of project", Option::None);
 /// ```
-pub fn warn_log(msg: &str) {
-    issue_command("warning", msg);
+pub fn warn_log(msg: &str, parameters: Option<LogParameters>) {
+    issue_command("warning", msg, parameters);
 }
 
 /// Creates an error message and prints the message to the log.
@@ -56,10 +89,10 @@ pub fn warn_log(msg: &str) {
 ///
 /// ```rust
 /// use actions_github::logger;
-/// logger::error_log("Did not find library");
+/// logger::error_log("Did not find library", Option::None);
 /// ```
-pub fn error_log(msg: &str) {
-    issue_command("error", msg);
+pub fn error_log(msg: &str, parameters: Option<LogParameters>) {
+    issue_command("error", msg, parameters);
 }
 
 /// Creates a notice message and prints the message to the log.
@@ -68,10 +101,10 @@ pub fn error_log(msg: &str) {
 ///
 /// ```rust
 /// use actions_github::logger;
-/// logger::notice_log("Step one is finished");
+/// logger::notice_log("Step one is finished", Option::None);
 /// ```
-pub fn notice_log(msg: &str) {
-    issue_command("notice", msg);
+pub fn notice_log(msg: &str, parameters: Option<LogParameters>) {
+    issue_command("notice", msg, parameters);
 }
 
 /// Returns if it's running on a debug runner.
