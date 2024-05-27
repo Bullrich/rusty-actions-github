56704af789ca01b94d9b0c6d57dba501e5b689ef
Javier Bullrich <javier@bullrich.dev>
2024-05-27 15:38:22 +0200

    added more benchmarks

--- benches/action_speed.rs
+++ benches/action_speed.rs
@@ -1,5 +1,6 @@
-use actions_github::core::set_output;
+use actions_github::core::{get_input, set_output};
 use actions_github::error::ActionsError;
+use actions_github::logger::{debug_log, error_log, info, is_debug, notice_log, warn_log};
 
 fn main() {
     // Run registered benchmarks.
@@ -12,4 +13,28 @@ fn set_output_benchmark(value: &str) {
         Ok(_) => {}
         Err(err) => panic!("{:#?}", err),
     }
+}
+
+#[divan::bench(args = ["name", "place", "owner"])]
+fn get_input_benchmark(value: &str) {
+    match get_input(value) {
+        Ok(_) => panic!("{} should not be available", value),
+        Err(_) => {}
+    }
+}
+
+#[divan::bench]
+fn get_debug_benchmark() {
+    if !is_debug() {
+        panic!("It should be debug")
+    }
+}
+
+#[divan::bench(args = ["hi", "example", "long words here"])]
+fn log_benchmark(msg: &str) {
+    debug_log(msg);
+    info(msg);
+    warn_log(msg);
+    error_log(msg);
+    notice_log(msg);
 }
